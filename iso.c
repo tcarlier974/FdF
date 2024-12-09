6889f495f034aebe8e0c352178e5ae79a63c5d6f
tcarlier <carliertiti97459@gmail.com>
2024-12-09 20:10:16 +0100

    auto

--- iso.c
+++ iso.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 19:50:30 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/12/09 20:09:28 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/12/09 20:10:13 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void	change_to_iso(t_tab ***tab, char *av)
 		x = 0;
 		while (x < l - 1)
 		{
-			printf("fff%d", tab[y][x]->x);
+			printf("fff%d\n", tab[y][x]->x);
 			tmpx = tab[y][x]->x;
 			tab[y][x]->x = isometric_format_x(tab[y][x]->x, tab[y][x]->y, tab[y][x]->z);
 			printf("x : %d  ", tab[y][x]->x);
