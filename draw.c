901bf7d081d795591fbf53357c2a1ad2764f8428
tcarlier <carliertiti97459@gmail.com>
2024-12-09 17:58:24 +0100

    auto

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:28:05 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/12/09 17:57:46 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/12/09 17:58:21 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static void	draw_line(t_tab tab1, t_tab tab2, t_data *img)
 	int sx;
 	int sy;
 	int err;
-
+	printf("test\n");
 	dx = abs(tab2.x - tab1.x), dy = -abs(tab2.y - tab1.y), err = dx + dy;
 	init_slope(&sx, &sy, &tab1, &tab2);
 	while (1)
@@ -60,7 +60,6 @@ void	*draw_line_img(t_data *img, t_tab **tab, char *av)
 	int y;
 
 	y = 0;
-	printf("test\n");
 	while (y < count_lines(av))
 	{
 		x = 0;
