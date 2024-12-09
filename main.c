eee111e6de31622b9beb323b30b6e5c7ef7edf03
tcarlier <carliertiti97459@gmail.com>
2024-12-09 16:49:00 +0100

    auto

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:47:50 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/12/09 16:41:55 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:48:58 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ void    *get_img(void *mlx, int width, int height, char *av)
 			tab[j][i].x = j + width / 4;
 			tab[j][i].y = i + height / 4;
 			tab[j][i].z = ft_atoi(ft_split(line, ' ')[i]);
-			tab[j][i].color = create_trgb(0, 255, 255, 255);
+			tab[j][i].color = create_trgb(0, 125, 0, 125);
 			i++;
 		}
 		free(line);
