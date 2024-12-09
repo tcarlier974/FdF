50519655bd0e94aae806488376298f22d3ee3eb7
tcarlier <carliertiti97459@gmail.com>
2024-12-09 16:24:23 +0100

    auto

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 17:47:50 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/12/09 16:23:00 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:24:20 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,6 @@ void    *get_img(void *mlx, int width, int height, char *av)
 		while (ft_split(line, ' ')[i])
 		{
 			tab[j].x = j + width / 4;
-			printf("test\n");
 			tab[j].y = i + height / 4;
 			tab[j].z = ft_atoi(ft_split(line, ' ')[i]);
 			tab[j].color = create_trgb(0, 255, 255, 255);
@@ -86,6 +85,7 @@ void    *get_img(void *mlx, int width, int height, char *av)
 		}
 		free(line);
 		j++;
+		i = 0;
 	}
 	close(fd);
 	return (draw_img(mlx, width, height, &tab, av));
