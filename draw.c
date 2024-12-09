aa43a3aa120df0b65b20c8b7184aea85b1dd3495
tcarlier <carliertiti97459@gmail.com>
2024-12-09 21:11:52 +0100

    auto

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:28:05 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/12/09 21:10:58 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/12/09 21:11:50 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,12 +50,12 @@ static void	draw_line(t_tab tab1, t_tab tab2, t_data *img)
 		printf("e2 : %d\n", e2);
 		printf("x1 : %d, y1 : %d\n", tab1.x, tab1.y);
 		printf("x2 : %d, y2 : %d\n", tab2.x, tab2.y);
-		if (e2 >= line.dy && tab1.x != tab2.x)
+		if ((e2 >= line.dy && tab1.x != tab2.x) || tab1.y == tab2.y)
 		{
 			line.err -= line.dy;
 			tab1.x += line.sx;
 		}
-		if (e2 <= line.dx)
+		if (e2 <= line.dx || tab1.x == tab2.x)
 		{
 			line.err += line.dx;
 			tab1.y += line.sy;
