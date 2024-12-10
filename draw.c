0f4b55846bfa516cba4cdcadb40cb114f14ff7ea
tcarlier <carliertiti97459@gmail.com>
2024-12-10 18:00:33 +0100

    auto

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:28:05 by tcarlier          #+#    #+#             */
-/*   Updated: 2024/12/10 17:59:51 by tcarlier         ###   ########.fr       */
+/*   Updated: 2024/12/10 18:00:31 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,9 +76,9 @@ void	*draw_line_img(t_data *img, t_tab **tab, char *av)
     while (y < l - 1)
     {
         x = 0;
-        while (x < c - 1)
+        while (x < c)
         {
-            if (x < c)
+            if (x < c - 1)
             {
                 printf("Drawing horizontal line at y=%d, x=%d to x+1=%d\n", y, x, x + 1);
                 draw_line(tab[y][x], tab[y][x + 1], &img);
